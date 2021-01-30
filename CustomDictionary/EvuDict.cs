ed5e65e744180b7e04129f53adf93772fbbdbe57
Emre Volkan UÇAR <emrevolkanucar@gmail.com>
2021-01-30 18:50:17 +0300

    Add project files.

---
+++ CustomDictionary/EvuDict.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDictionary
+{
+    class EvuDict<DictKeyType, DictValType>
+    {
+
+        DictKeyType[] _dictKeyTypes;
+        DictValType[] _dictValTypes;
+
+        DictKeyType[] _tempDictKeyTypes;
+        DictValType[] _tempDictValTypes;
+
+        public EvuDict()
+        {
+            _dictKeyTypes = new DictKeyType[0];
+            _dictValTypes = new DictValType[0];
+        }
+
+        public void Add(DictKeyType item1, DictValType item2)
+        {
+
+            _tempDictKeyTypes = _dictKeyTypes;
+            _tempDictValTypes = _dictValTypes;
+
+            _dictKeyTypes = new DictKeyType[_dictKeyTypes.Length + 1];
+            _dictValTypes = new DictValType[_dictValTypes.Length + 1];
+
+            for (int i = 0; i < _tempDictKeyTypes.Length; i++)
+            {
+                _dictKeyTypes[i] = _tempDictKeyTypes[i];
+            }
+            _dictKeyTypes[_dictKeyTypes.Length - 1] = item1;
+
+
+            for (int i = 0; i < _tempDictValTypes.Length; i++)
+            {
+                _dictValTypes[i] = _tempDictValTypes[i];
+            }
+            _dictValTypes[_dictValTypes.Length - 1] = item2;
+
+        }
+
+        public DictKeyType[] Keys
+        {
+            get { return _dictKeyTypes; } //Set etmesini istemedik çünkü ReadOnly
+        }
+
+        public DictValType[] Values
+        {
+            get { return _dictValTypes; } //Set etmesini istemedik çünkü ReadOnly
+        }
+    }
+}
---
+++ CustomDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EvuDict<string, int> plakaKodlari = new EvuDict<string, int>();
+            plakaKodlari.Add("İstanbul", 34);
+            plakaKodlari.Add("Ankara", 06);
+            plakaKodlari.Add("İzmir", 35);
+
+            Console.WriteLine("Şehir: " + plakaKodlari.Keys[0] + " | İl Plaka Kodu: " + plakaKodlari.Values[0]  );
+        }
+    }
+}
